c5eb5cc42764ae1065024318670bfa1f5411287c
zewais <arch.ziad.ewais@gmail.com>
2025-02-06 22:25:21 -0500

    Add userprofile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Greeting from "./components/Greeting";
 import Counter from "./components/Counter";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
       <Greeting name="Alice" />
       <Counter />
+      <UserProfile userId={2} />
     </>
   );
 }
